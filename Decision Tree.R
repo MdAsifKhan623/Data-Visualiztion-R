98d620523e566fcec82df8048e0d28e5d3ab3688
Asif623 <mdasifk22@gmail.com>
2021-02-24 12:53:30 -0500

    some changes to decision tree along with prediction

--- Decision Tree.R
+++ Decision Tree.R
@@ -1,23 +1,27 @@
 library(rpart)
 library(rpart.plot)
 
-
-
 #importing the dataset
 data=read.csv("C:\\MS\\Spring 2021\\Data Visualization CAP 5738\\dataset.csv")
+
+data=data[1:300,]
 
 data
 
 dim(data)
 
-smple<- sample(200,80)
+smple<- sample(300,120)
 
 training_data<- data[smple,]
 test_data<- data[-smple,]
 
 decisionTreeModel<- rpart(Index~., training_data, method = "class")
 
+
+
 rpart.plot(decisionTreeModel)
 
-predictedValue= predict(decisionTreeModel,test_data[,4])
+predictedValue= predict(decisionTreeModel,newdata=test_data[,1:3],type="class")
+
+predictedValue
 
